ab1f506193f45229263a835059dda732dad1dd9b
Kai Brooks <kaibrooks22@gmail.com>
2017-10-02 09:50:10 -0700

    Drew 2 circles  filled with color

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 // Zombulator by Kai Brooks
 
 function setup() {
-
+createCanvas(800,800)
 }
 
 function draw() {
+	fill(255, 0, 0)
   ellipse(50, 50, 80, 80);
+  fill(150, 30, 30)
+  ellipse(200, 100, 80, 80);
 }
