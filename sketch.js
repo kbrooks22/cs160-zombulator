af051cf17c2556b48395ff32e220bd154844166b
Kai Brooks <kaibrooks22@gmail.com>
2017-10-04 13:43:32 -0700

    Voided Code that made circle follow cursor
    Attempting to make circles move on own. Unsuccessful so far. the code matches that of Yong's so just going to delete all and start again

--- sketch.js
+++ sketch.js
@@ -1,16 +1,26 @@
 // Zombulator by Kai Brooks
+ var zombieX = 50;
+ var zombie2X = 100; 
 
 function setup() {
 createCanvas(800,800)
 }
 
+// function draw() {
+// 	if (mouseIsPressed) {
+// 		fill(0);
+// 	} else {
+// 		fill(255);
+// 	}
+// 	ellipse(mouseX, mouseY, 20,20);
+
+
 function draw() {
-	if (mouseIsPressed) {
-		fill(0);
-	} else {
-		fill(255);
-	}
-	ellipse(mouseX, mouseY, 20,20);
+	 background(255, 255, 255);
+	 fill(134, 10, 127);
+	 ellipse(50, 50, 80, 80);
+	 fill(150, 150, 150);
+	 ellipse(200, 100, 80, 80);
+	 zombieX = zombieX + 1; 
+	 zombie2X = zombie2X + 3; 
 }
-
-
