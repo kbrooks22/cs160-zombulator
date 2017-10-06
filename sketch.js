8077f246ff4ddf8e0daf32e240804d085a3acd5f
Kai Brooks <kaibrooks22@gmail.com>
2017-10-06 09:56:29 -0700

    Added border color to circles

--- sketch.js
+++ sketch.js
@@ -1,17 +1,25 @@
 // Zombulator by Kai Brooks
 
 var zombiex = 80; 
-var zombie2 = 80; 
+var zombie2 = 200; 
 
 function setup() {
 	createCanvas(800, 800);
 }
 
 function draw() {
+	background(255, 255, 255)
+	strokeWeight(8);
+	stroke(79, 70, 80)
 	fill(150, 20, 20);
 	ellipse(zombiex, 50, 80,80);
 	fill(200, 200, 15);
 	ellipse(zombie2, 70, 80, 80);
 	zombiex = zombiex + 1; 
-	zombie2 = zombie2 + 6;
+	zombie2 = zombie2 + 6; 
+if (zombie2 >= 800 ) {
+		zombie2 = 0; 
+if (zombiex >= 400) {
+	zombiex = 0; 
 }
+	}}
