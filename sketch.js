98ee6bcc603b5dd9b145c7fd7e4d246ac1e0542d
Kai Brooks <kaibrooks22@gmail.com>
2017-10-05 20:09:57 -0700

    Deleted all code restarted
    The circles now move based on var zombiex and zombie 2

--- sketch.js
+++ sketch.js
@@ -1,26 +1,17 @@
 // Zombulator by Kai Brooks
- var zombieX = 50;
- var zombie2X = 100; 
+
+var zombiex = 80; 
+var zombie2 = 80; 
 
 function setup() {
-createCanvas(800,800)
+	createCanvas(800, 800);
 }
 
-// function draw() {
-// 	if (mouseIsPressed) {
-// 		fill(0);
-// 	} else {
-// 		fill(255);
-// 	}
-// 	ellipse(mouseX, mouseY, 20,20);
-
-
 function draw() {
-	 background(255, 255, 255);
-	 fill(134, 10, 127);
-	 ellipse(50, 50, 80, 80);
-	 fill(150, 150, 150);
-	 ellipse(200, 100, 80, 80);
-	 zombieX = zombieX + 1; 
-	 zombie2X = zombie2X + 3; 
+	fill(150, 20, 20);
+	ellipse(zombiex, 50, 80,80);
+	fill(200, 200, 15);
+	ellipse(zombie2, 70, 80, 80);
+	zombiex = zombiex + 1; 
+	zombie2 = zombie2 + 6;
 }
