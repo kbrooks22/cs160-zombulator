ebedb2276ea7f3f87c075d05a76e124c2c0e4480
Kai Brooks <kaibrooks22@gmail.com>
2017-10-11 17:10:13 -0700

    Started to declare functions
    Half way through, succesfullly have two balls bounce off eachother and the other set bouncing off of window

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
 // Zombulator by Kai Brooks
 
 var zombieY = 15;
+var zombieY2 = 10;
 var zombieV = 0;
 var zombieA = 0.2;
 var zombieDamping = -0.8;
 var zombieColor;
 var backgroundColor; 
 var zombiesize = 80; 
-var humanY =550;
+var humanY = 550;
+var humanY2 = 570;
 var humanColor;
 var humanSize = 60;
 var humanV = 0;
@@ -24,23 +26,53 @@ function setup() {
 function draw() {
 	background(backgroundColor);
 	noStroke();
+
+	drawZombie();
+	moveZombie();
+	drawHuman();
+	moveHuman();
+
 	fill(zombieColor);
-	ellipse( windowWidth / 2, zombieY, zombiesize, zombiesize);
+	ellipse( windowWidth / 3, zombieY2, zombiesize, zombiesize);
 	fill(humanColor);
-	ellipse( windowWidth / 2, humanY, humanSize, humanSize);
+	ellipse( windowWidth / 3, humanY2, humanSize, humanSize);
+	humanY2 -= humanV;
+	zombieY2 += zombieV;
 	humanY -= humanV;
 	humanV += humanA;
-	if (humanY - (humanSize / 2) <= windowHeight * 0) {
-		humanY = (windowHeight * 0) + (humanSize / 2);
+	if (humanY2 - (humanSize / 2) <= zombieY2) {
 		humanV *= humanDamping;
+	 }
+	if (zombieY2 + (zombiesize / 2) >= humanY2) {
+	 	zombieV = zombieV * -.8;
+ }
 	}
+
+function drawZombie() {
+	fill(zombieColor);
+	ellipse( windowWidth / 2, zombieY, zombiesize, zombiesize);
+}
+
+function moveZombie() {
 	zombieY += zombieV;
 	zombieV += zombieA;
 	if (zombieY + (zombiesize / 2) >= windowHeight) {
 		zombieY = windowHeight - (zombiesize / 2); 
 		zombieV = zombieV * -.8;
 	} 
+}
+function drawHuman() {
+	fill(humanColor);
+	ellipse( windowWidth / 2, humanY, humanSize, humanSize);
+	fill(0);
+	text("human", windowWidth / 2, humanY);
+}
+
+function moveHuman() {
+	humanY -= humanV;
+	humanV += humanA;
+	if (humanY - (humanSize / 2) <= windowHeight * 0) {
+		humanY = (windowHeight * 0) + (humanSize / 2);
+		humanV *= humanDamping;
 	}
-
-
-
+}
