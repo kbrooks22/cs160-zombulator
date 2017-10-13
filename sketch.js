5031dcb5b130b985403891cb6d503bdde95fba01
Kai Brooks <kaibrooks22@gmail.com>
2017-10-12 21:00:47 -0700

    Converted Draw code to a series of functions
    Successfully have two indepent sets of balls

--- sketch.js
+++ sketch.js
@@ -1,20 +1,28 @@
 // Zombulator by Kai Brooks
 
 var zombieY = 15;
-var zombieY2 = 10;
 var zombieV = 0;
 var zombieA = 0.2;
 var zombieDamping = -0.8;
 var zombieColor;
+var zombiesize = 80;
+
+var zombieY2 = 10;
+var zombieV2 = 0;
+var zombieA2 = .15;
+
 var backgroundColor; 
-var zombiesize = 80; 
+
 var humanY = 550;
-var humanY2 = 570;
 var humanColor;
 var humanSize = 60;
 var humanV = 0;
 var humanA = 0.2;
 var humanDamping = -0.7;
+
+var humanY2 = 570;
+var humanV2 = 0;
+var humanA2 = .23;
 
 function setup() {
 	createCanvas(windowWidth , windowHeight);
@@ -29,23 +37,29 @@ function draw() {
 
 	drawZombie();
 	moveZombie();
+	drawZombie2();
+	moveZombie2();
 	drawHuman();
 	moveHuman();
+	drawHuman2();
+	moveHuman2();
 
-	fill(zombieColor);
-	ellipse( windowWidth / 3, zombieY2, zombiesize, zombiesize);
-	fill(humanColor);
-	ellipse( windowWidth / 3, humanY2, humanSize, humanSize);
-	humanY2 -= humanV;
-	zombieY2 += zombieV;
-	humanY -= humanV;
-	humanV += humanA;
-	if (humanY2 - (humanSize / 2) <= zombieY2) {
-		humanV *= humanDamping;
-	 }
-	if (zombieY2 + (zombiesize / 2) >= humanY2) {
-	 	zombieV = zombieV * -.8;
- }
+	// fill(zombieColor);
+	// ellipse( windowWidth / 3, zombieY2, zombiesize, zombiesize);
+	// fill(humanColor);
+	// ellipse( windowWidth / 3, humanY2, humanSize, humanSize);
+	// humanY2 -= humanV2;
+	// humanV2 += humanA2;
+	// zombieY2 += zombieV2;
+	// zombieV2 += zombieA2;
+	// humanY -= humanV;
+	// humanV += humanA;
+	// if (humanY2 - (humanSize / 2) <= zombieY2) {
+		// humanV2 *= humanDamping;
+	 // }
+	// if (zombieY2 + (zombiesize / 2) >= humanY2) {
+	 	// zombieV2 = zombieV * -.8;
+ // }
 	}
 
 function drawZombie() {
@@ -61,6 +75,20 @@ function moveZombie() {
 		zombieV = zombieV * -.8;
 	} 
 }
+
+function drawZombie2() {
+	fill(zombieColor);
+	ellipse( windowWidth / 3, zombieY2, zombiesize, zombiesize);
+}
+
+function moveZombie2() {
+	zombieY2 += zombieV2;
+	zombieV2 += zombieA2;
+	if (zombieY2 + (zombiesize / 2) >= humanY2) {
+	 	zombieV2 = zombieV * -.8;
+ }
+}
+
 function drawHuman() {
 	fill(humanColor);
 	ellipse( windowWidth / 2, humanY, humanSize, humanSize);
@@ -75,4 +103,17 @@ function moveHuman() {
 		humanY = (windowHeight * 0) + (humanSize / 2);
 		humanV *= humanDamping;
 	}
+}
+
+function drawHuman2 () {
+	fill(humanColor);
+	ellipse( windowWidth / 3, humanY2, humanSize, humanSize);
+}
+
+function moveHuman2() {
+	humanY2 -= humanV2;
+	humanV2 += humanA2;
+	if (humanY2 - (humanSize / 2) <= zombieY2) {
+		humanV2 *= humanDamping;
+	 }
 }
