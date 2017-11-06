8baf12f8148a45040bda8e5978292e7af3a7db9b
Kai Brooks <kaibrooks22@gmail.com>
2017-11-05 19:16:58 -0800

    Code now creates a Population, rather than zombies and humans
    Started to implent fight function, only did code up to end of class video

--- sketch.js
+++ sketch.js
@@ -1,98 +1,124 @@
 // Zombulator by Kai Brooks
-
-
 var backgroundColor;
 
-const MIN_SIZE = 25; 
+const MIN_SIZE = 5;
 const MAX_SIZE = 50;
-const Number_Of_Zombies = 100;
-const Number_Of_Humans = 100;
+const POPULATION_SIZE = 500;
 
-var zombies;
+var population = [];
 
-var humans;
+var zombieCount = 0;
+var humanCount = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   backgroundColor = color(245, 255, 245);
-  initializeZombies();
-  initializeHumans();
+  initializePopulation();
 }
 
 function draw() {
   background(backgroundColor);
   noStroke();
-
-  drawZombies();
-  drawHumans();
-  moveHumans();
+  drawPopulation();
+  movePopulation();
+  drawPopulationCounts();
+  handlecollisions();
 }
 
-function initializeZombies() {
-zombies = [];
-for (var i = 0; i < Number_Of_Zombies; ++i) {
-  zombies[i] = initializeZombie();
-  }
-}
-
-
-function initializeZombie() {  
-return{
-  	x: random(0, windowWidth),
-  	y: random(0, 200),
-  	size: random(MIN_SIZE, MAX_SIZE),
-  	color: color(random(0, 150), random(0, 255), random(0, 255), 150)
- 	draw: function() {
-    fill(this.color);
-    ellipse(this.x, this.y, this.size, this.size);
-   };
+function handlecollisions() {
+for (var i = 0; i < POPULATION_SIZE; ++i) {
+	var attacker = population[i];
+	for (var j = i + 1; j < POPULATION_SIZE; ++j)
+		var target = population[i];
+	if (attacker.isZombie() && target.isHuman() && attacker.isTouching(target))  {
+		print("FIGHT!, FIGHT!, FIGHT!");
+	}
 }
 }
 
-function drawZombies() {
- for (var i = 0; i <Number_Of_Zombies; ++i) {
-	 zombies[i].draw();
- }
- }
-
-
-function initializeHumans() {
-   humans = [];
-for (var i = 0; i < Number_Of_Humans; ++i) {
-	humans[i] = initializeHuman();
- }
- }
-
- function initializeHuman () {
- return{
- 	x:random(0, windowWidth),
- 	y:random(400, 500),
- 	size:random(MIN_SIZE, MAX_SIZE),
- 	color:color(random(255), random( 150, 255), random(150, 255), 150),
- 	speed: 0.1,
- 	draw: function() {
-    fill(this.color);
-    ellipse(this.x, this.y, this.size, this.size);
-   },
-    move: function() {
-    var VY = this.y + this.speed;
-    var NY = noise(VY);
-    this.y -= NY;
-
+function initializePopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    var humanoid_type = random(0, 100);
+    if (humanoid_type <= 50) {
+      population[i] = initializeZombie();
+      ++zombieCount;
+    } else {
+      population[i] = initializeHuman();
+      ++humanCount;
     }
- }
-}
-
-
-function drawHumans() {
-for (var i = 0; i <Number_Of_Humans; ++i) {
-	humans[i].draw();
-}
-}
-
-function moveHumans() {
-for (var i = 0; i <Number_Of_Humans; ++i) {
-	humans[i].move();
-} 
   }
+}
 
+function drawPopulationCounts() {
+  stroke(0);
+  textSize(72);
+  textAlign(CENTER);
+  text("Zombies: " + zombieCount, width / 2, 100);
+  text("Humans: " + humanCount, width / 2, height - 100);
+}
+
+function drawPopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    population[i].draw();
+  }
+}
+
+function movePopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    population[i].move();
+  }
+}
+
+function initializeZombie() {
+  return {
+    x: random(0, windowWidth),
+    y: random(0, 200),
+    speed: random(0.25, 3),
+    size: random(MIN_SIZE, MAX_SIZE),
+    color: color(random(100, 255), random(50, 150), random(50, 150), 150),
+    move: function() {
+      var direction = random(0, 100);
+      if (direction < 20) {
+        this.x += this.speed;
+      } else if (direction < 40) {
+        this.x -= this.speed;
+      } else if (direction < 60) {
+        this.y -= this.speed;
+      } else {
+        this.y += this.speed;
+      }
+    },
+    draw: function() {
+      fill(this.color);
+      ellipse(this.x, this.y, this.size, this.size);
+    }
+  };
+}
+
+function initializeHuman() {
+  return {
+    x: random(0, windowWidth),
+    y: random(windowHeight - 200, windowHeight),
+    speed: random(0.25, 3),
+    size: random(MIN_SIZE, MAX_SIZE),
+    color: color(random(50, 150), random(50, 150), random(150, 255), 150),
+    move: function() {
+        var direction = random(0, 100);
+        if (direction < 20) {
+          this.x += this.speed;
+        } else if (direction < 40) {
+          this.x -= this.speed;
+        } else if (direction < 60) {
+          this.y += this.speed;
+        } else {
+          this.y -= this.speed;
+        }
+      },
+    draw: function() {
+        fill(this.color);
+        ellipse(this.x, this.y, this.size, this.size);
+    },
+    isTouching: function() {
+    	}
+  };
+}
